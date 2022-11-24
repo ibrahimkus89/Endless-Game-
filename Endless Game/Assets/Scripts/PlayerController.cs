65a10d33983d071c2e19332abedc444294f820c5
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2022-11-24 14:27:23 +0300

    moving th player right and left

--- Endless Game/Assets/Scripts/PlayerController.cs
+++ Endless Game/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     CharacterController controller;
     Vector3 direction;
     [SerializeField] float forwardSpeed;
+    int desiredLane = 1;
+    [SerializeField] float laneDistance = 4;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -16,6 +18,39 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+
+            if (desiredLane ==3)
+            {
+                desiredLane = 2;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (desiredLane ==0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane ==2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position,targetPosition,80 * Time.fixedDeltaTime);
     }
 
     private void FixedUpdate()
